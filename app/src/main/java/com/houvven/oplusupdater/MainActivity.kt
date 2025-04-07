5d83cb28f3c98a6bbc8e0c34db5ddfdc880fa14a
houvven <houvven@gmail.com>
2025-04-07 17:27:41 +0800

    refactor(theme): remove hardcoded color values from light and dark color schemes

--- app/src/main/java/com/houvven/oplusupdater/MainActivity.kt
+++ app/src/main/java/com/houvven/oplusupdater/MainActivity.kt
@@ -5,20 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.ui.graphics.Color
 import com.houvven.oplusupdater.ui.screen.home.HomeScreen
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.theme.darkColorScheme
 import top.yukonga.miuix.kmp.theme.lightColorScheme
 
 private val lightColor = lightColorScheme(
-    primary = Color(0xFF5163FB),
-    disabledPrimaryButton = Color(0xff8891da),
 )
 
 private val darkColor = darkColorScheme(
-    primary = Color(0xff3544c6),
-    disabledPrimaryButton = Color(0xff7276a3)
 )
 
 
