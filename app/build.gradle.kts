829314447147db36914f16159c2e1d9dfc01fff3
houvven <houvven@gmail.com>
2025-10-21 18:10:42 +0800

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.houvven.oplusupdater"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.houvven.oplusupdater"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 7
         versionName = "1.0.6"
 
